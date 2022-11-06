9862a251199b12a49779a3308865e4dff7aa5d36
Your Namepythonvista <you@example.comnablintech@gmail.com>
2022-11-06 18:35:01 +0100

    updated the app

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="wrap px-4 py-12 flex flex-col justify-end">
-    <div class="btns px-4 w-full flex flex-col items-start justify-end gap-3">
+  <div class="wrap px-4 py-6 flex flex-col justify-end">
+    <div class="btns  w-full flex flex-col items-start justify-end gap-3">
         <p class="ma-0 pa-0 text-white text-2xl font-bold">Welcome to</p>
         <img src="@/assets/img/logo.png" alt="">
-        <v-btn large class="w-full btn" color="white"><span class="text-lg font-bold">Login</span></v-btn>
-        <v-btn large color="#00DC48" class="w-full btn " ><span class="text-lg font-bold">Create an Account</span></v-btn>
+        <v-btn large class="w-full btn" color="white">Login</v-btn>
+        <v-btn large color="#00DC48" class="w-full btn " >Create an Account</v-btn>
     </div>
 
   </div>
