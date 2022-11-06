c4185dc593d74aec92ffbcbf067775caabc447d8
Your Namepythonvista <you@example.comnablintech@gmail.com>
2022-11-06 18:37:13 +0100

    updated the app

--- vue.config.js
+++ vue.config.js
@@ -3,15 +3,15 @@ module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
   pwa: {
     name: "Ubclicks",
-    themeColor: "#4DBA87",
+    themeColor: "#00DC48",
     msTileColor: "#000000",
     iconPaths: {
       faviconSVG: "img/icons/favicon.svg",
       favicon32: "img/icons/favicon-32x32.png",
       favicon16: "img/icons/favicon-16x16.png",
       appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
-      maskIcon: "img/icons/safari-pinned-tab.svg",
-      msTileImage: "img/icons/msapplication-icon-144x144.png",
+      maskIcon: "img/icons/logo.png",
+      msTileImage: "img/icons/logo.png",
     },
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black",
