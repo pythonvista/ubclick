97cf1651102f346a553a0397ab5c16ac6df44b42
Your Namepythonvista <you@example.comnablintech@gmail.com>
2023-01-08 17:13:06 +0100

    update the logo

--- vue.config.js
+++ vue.config.js
@@ -16,11 +16,5 @@ module.exports = defineConfig({
     appleMobileWebAppCapable: "yes",
     appleMobileWebAppStatusBarStyle: "black",
     // configure the workbox plugin
-    workboxPluginMode: "InjectManifest",
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: "./src/service-worker.js",
-      // ...other Workbox options...
-    },
   },
 });
