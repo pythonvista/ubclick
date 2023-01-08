8ee72ee262b2c92c41ed4213e57373ac27dcb22f
Your Namepythonvista <you@example.comnablintech@gmail.com>
2023-01-08 16:59:09 +0100

    update the logo

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,11 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
   pwa: {
-    name: "Ubclicks",
+    name: "UbClick",
     themeColor: "#00DC48",
     msTileColor: "#000000",
     iconPaths: {
-      faviconSVG: "img/icons/favicon.svg",
+      faviconSVG: "img/icons/favicon.png",
       favicon32: "img/icons/favicon-32x32.png",
       favicon16: "img/icons/favicon-16x16.png",
       appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
