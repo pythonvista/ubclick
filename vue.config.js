756355cb4ef5222c02332abbbb78c28c27b4a68f
Your Namepythonvista <you@example.comnablintech@gmail.com>
2023-01-08 17:20:53 +0100

    update the logo

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     themeColor: "#00DC48",
     msTileColor: "#000000",
     iconPaths: {
-      faviconSVG: "img/icons/favicon.png",
+      faviconSVG: "img/icons/favicon.svg",
       favicon32: "img/icons/favicon-32x32.png",
       favicon16: "img/icons/favicon-16x16.png",
       appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
