74fa76f91edc1a166f93769b7a28354b55ff266f
Your Namepythonvista <you@example.comnablintech@gmail.com>
2023-03-26 15:04:16 +0100

    updated

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig({
       faviconSVG: "img/icons/favicon.svg",
       favicon32: "img/icons/android/android-launchericon-48-48.png",
       favicon16: "img/icons/favicon-16x16.png",
-      appleTouchIcon: "img/icons/apple-touch-icon-152x152.png",
+      appleTouchIcon: "img/icons/android/android-launchericon-192-192.png",
       maskIcon: "img/icons/android/android-launchericon-192-192.png",
       msTileImage: "img/icons/android/android-launchericon-512-512.png",
     },
